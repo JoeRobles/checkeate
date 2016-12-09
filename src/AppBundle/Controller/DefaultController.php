119f3d878d096e656807e1f46512eac5543377a8
Joe Robles <joe.robles.pdj@gmail.com>
2016-12-09 00:08:32 -0500

    implemented book download

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -6,6 +6,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Culqi\Culqi;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
 
 class DefaultController extends Controller
 {
@@ -14,7 +15,9 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        return $this->render('default/index.html.twig', array());
+        $orden = uniqid(mt_rand(), true);
+
+        return $this->render('default/index.html.twig', array('orden' => $orden));
     }
 
     /**
@@ -30,35 +33,50 @@ class DefaultController extends Controller
      */
     public function verifyAction(Request $request)
     {
-        $culqi = new Culqi(array('api_key' => $this->get('secret_api_key')));
+        $culqi = new Culqi(array(
+            'api_key' => $this->container->getParameter('secret_api_key')
+        ));
         $culqi->setEnv("INTEG");
+        $token = $request->request->get('token');
+        $datos = $request->request->get('datos');
+        $datos = json_decode($datos);
 
-        $request->request->get('token');
+        if ($token) {
+            try {
+                $cargo = $culqi->Cargos->create(array(
+                    "token"=> $token,
+                    "moneda"=> "PEN",
+                    "monto"=> 200,
+                    "descripcion"=> $datos->descripcion,
+                    "pedido"=> $datos->orden,
+                    "codigo_pais"=> "PE",
+                    "ciudad"=> "Lima",
+                    "usuario"=> "71701956",
+                    "direccion"=> "Avenida Lima 1232",
+                    "telefono"=> 3333339,
+                    "nombres"=> $datos->nombre,
+                    "apellidos"=> $datos->apellido,
+                    "correo_electronico"=> $datos->correo_electronico
+                ));
 
-        $pedidoId = sha1(uniqid(mt_rand(), true));
-
-        try {
-            $cargo = $culqi->Cargos->create(array(
-                "moneda"=> "PEN",
-                "monto"=> 19900,
-                "usuario"=> "71701956",
-                "descripcion"=> "Venta de prueba",
-                "pedido"=> $pedidoId,
-                "codigo_pais"=> "PE",
-                "direccion"=> "Avenida Lima 1232",
-                "ciudad"=> "Lima",
-                "telefono"=> 3333339,
-                "nombres"=> "Brayan",
-                "apellidos"=> "Cruces",
-                "correo_electronico"=> "brayan.cruces@culqi.com",
-                "token"=> "vJk6e1LIoZLdDwEXTE6KMQlaJvqswSwU"
-            ));
-        } catch(Exception $e) {
-            // ERROR: El cargo tuvo algún error o fue rechazado
-            echo $e->getMessage();
-
+                if (isset($cargo->id)) {
+                    return $this->render('default/verify.html.twig', array('cargo' => $cargo));
+                } else {
+                    return $this->render('default/verify.html.twig', array('cargo' => $cargo));
+                }
+            } catch(Exception $e) {
+                return $this->render('default/verify.html.twig', array('mensaje' => $e->getMessage()));
+            }
         }
 
         return $this->render('default/verify.html.twig', array());
+    }
+
+    /**
+     * @Route("/libro", name="libro")
+     */
+    public function libroAction(Request $request)
+    {
+        return new BinaryFileResponse(__DIR__ . '/../Resources/private/Demasiado-inteligente-para-ser-feliz-Jeanne-Siaud-Facchin.pdf');
     }
 }
